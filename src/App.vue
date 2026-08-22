beee552378270ccc84877accc95a7bc6436ccbaa
Marcos Alexandr Nogueira Rocha <alxnrocha@users.noreply.github.com>
2026-08-22 21:24:44 +0200

    revert(ui): completely remove author project badge

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import ProjectBadge from './components/ProjectBadge.vue';
 import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div class="min-h-screen bg-[#0b0f17] text-gray-100 flex flex-col font-sans">
     <RouterView />
-      <ProjectBadge />
   </div>
 </template>
